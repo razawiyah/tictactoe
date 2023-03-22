fb3c2ff9bc011f3a253ef5fe29282a2ea23348c5
Rabiatul Adawiyah ZA <rabiatul.nraza@gmail.com>
2023-03-22 15:55:00 +0800

    sign up n login interface

--- app/src/main/java/com/example/exerciseprescription/Login.java
+++ app/src/main/java/com/example/exerciseprescription/Login.java
@@ -2,13 +2,28 @@ package com.example.exerciseprescription;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    TextView signupTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signupTV = findViewById(R.id.signupTV);
+
+        signupTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, SignUp.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/exerciseprescription/SignUp.java
+++ app/src/main/java/com/example/exerciseprescription/SignUp.java
@@ -2,13 +2,37 @@ package com.example.exerciseprescription;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
 
+    TextView loginTV,safetyTV;
+    public SafetyPrecaution safetyPrecaution;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        loginTV = findViewById(R.id.loginTV);
+        safetyPrecaution = new SafetyPrecaution(this);
+        safetyTV = findViewById(R.id.safetyTV);
+
+        loginTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUp.this, Login.class));
+                finish();
+            }
+        });
+
+        safetyTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                safetyPrecaution.show();
+            }
+        });
     }
 }
